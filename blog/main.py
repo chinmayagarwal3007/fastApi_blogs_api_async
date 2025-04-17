0eb3e83643c527e2947adf77db516c4ac1527a5c
Chinmay <agarwalchinmay3007@gmail.com>
2025-04-17 19:38:51 +0530

    basic crud

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from .schemas import Blog as BlogSchema
 from .models import Blog as BlogModel
@@ -16,7 +16,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: BlogSchema, db: Session = Depends(get_db)):
     new_blog = BlogModel(title=request.title, body=request.body)
     db.add(new_blog)
@@ -28,3 +28,31 @@ def create(request: BlogSchema, db: Session = Depends(get_db)):
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(BlogModel).all()
     return blogs
+
+@app.get("/blog/{id}")
+def show_blog(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"error": "Blog not found"}
+    return blog
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)   
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    db.delete(blog)
+    db.commit()
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, request: BlogSchema, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    db.refresh(blog)
+    return {"Updated_blog": blog}  
