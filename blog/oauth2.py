413d71ff4d462262ca0ed4d77e3963b4729b94e8
Chinmay Agarwal <76059604+chinmayagarwal3007@users.noreply.github.com>
2025-04-22 11:51:31 +0530

    Create oauth2.py

---
+++ blog/oauth2.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from ..token import SECRET_KEY, ALOGORITHM
+from jose import jwt
+from ..schemas import TokenData
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
