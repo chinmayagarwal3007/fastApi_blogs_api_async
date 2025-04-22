43cee8e0ce368598cef47ef0e6cbebdc0c500238
Chinmay <agarwalchinmay3007@gmail.com>
2025-04-22 09:39:00 +0530

    add login

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,11 +1,28 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-
+from ..schemas import Login as LoginSchema
+from ..db import get_db
+from sqlalchemy.orm import Session
+from ..models import User
+from passlib.context import CryptContext
+from datetime import timedelta
+from ..token import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from ..schemas import Token
 
 router = APIRouter(
-    tags=["authentication"],
-    prefix="/auth"
+    tags=["authentication"]
 )
 
+pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated = "auto")
+
 @router.post("/login")
-def login():
-    return {"message": "Login successful"}
+def login(request: LoginSchema, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    if pwd_cxt.verify(request.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
--- blog/schemas.py
+++ blog/schemas.py
@@ -35,3 +35,15 @@ class ShowBlog(BaseModel):
     class Confing():
         orm_mode = True
 
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
