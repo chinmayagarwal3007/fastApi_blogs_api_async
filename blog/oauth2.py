598da0031c4594caa7cb5922dbff04abf0897004
Chinmay Agarwal <76059604+chinmayagarwal3007@users.noreply.github.com>
2025-04-22 15:25:18 +0530

    Update oauth2.py

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -5,7 +5,7 @@ from jose import jwt
 from typing import Annotated
 from .schemas import TokenData
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
