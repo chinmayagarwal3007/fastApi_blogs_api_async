c976d85c2760473596c82f3f7e5a572751c9a3dd
Chinmay Agarwal <76059604+chinmayagarwal3007@users.noreply.github.com>
2025-04-22 15:27:17 +0530

    Update authentication.py

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -16,7 +16,7 @@ router = APIRouter(
 pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated = "auto")
 
 @router.post("/login")
-def login(request: OAuth2PasswordRequestForm, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
